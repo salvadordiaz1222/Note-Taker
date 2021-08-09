a8ba3800d1704757a0b02cb564d211524f806d2a
salvadordiaz1222 <salvador.diaz1222@gmail.com>
2021-08-09 11:53:20 -0500

    Folders in the correct location

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const theDB = require("./develop/db/db.json");
 const { v4: uuidv4 } = require("uuid");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const fs = require("fs");
 const util = require("util");
 
